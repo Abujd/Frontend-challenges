5ae956bcc7a72c20452e0b746dd15b6f818ed74a
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2022-09-06 23:25:03 +0530

    Initial setup for react form validator

---
+++ react-form-validator/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormValidator from './component/formValidator';
+
+function App() {
+  return (
+   <>
+    <FormValidator />
+   </>
+  );
+}
+
+export default App;
